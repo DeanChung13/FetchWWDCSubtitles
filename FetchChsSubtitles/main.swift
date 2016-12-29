517ac69e01e445776c474f0556e54729b9d8d832
Dean.Chung <deanchung@homemac.com>
2016-12-29 18:27:13 +0800

    Initial Commit

---
+++ FetchChsSubtitles/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FetchChsSubtitles
+//
+//  Created by Dean Chung on 29/12/2016.
+//  Copyright © 2016 Dean Chung. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
